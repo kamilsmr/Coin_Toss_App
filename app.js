1305534630bb03e1e17463033f42e0384b1d3080
kamilsmr <kamils6035@gmail.com>
2022-10-17 10:12:15 +0300

    finish

--- app.js
+++ app.js
@@ -1,24 +1,25 @@
-const coinArray = ["Heads","Tails"];
+const coinArray = ["Heads", "Tails"];
+let score = [0, 0];
 const message = document.querySelector(".message");
-const buttons = document.querySelectorAll("button");
-
-for(let i =0; i<buttons.length; i++){
-console.log(buttons[i]);
-buttons[i].addEventListener("click",lossCoin)
+const buttons = document.querySelectorAll("button")
+for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener("click", tossCoin);
 }
-
-function lossCoin(e){
-    // console.log("player:" + e.target.innetText);
+function tossCoin(e) {
     let playerGuess = e.target.innerText;
-    let computerToss = Math.floor(Math.random()*2);
+    let computerToss = Math.floor(Math.random() * coinArray.length);
     let computerGuess = coinArray[computerToss];
-    // console.log("computer:" + coinArray[computerToss]);
-    if(playerGuess){
-
-    }else{
-
+    message.innerHTML = "Computer Selected " + computerGuess + "<br>";
+    let output;
+    if (playerGuess === computerGuess) {
+        output = "Player Wins";
+        score[0]++;
     }
+    else {
+        output = "Computer Wins";
+        score[1]++;
+    }
+    message.innerHTML += output + "<br>Player " + score[0] + " Computer " + score[1];
 }
-
 
 
