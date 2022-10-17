9a5f7018c56a0700456e40a4485a5dd492d0e532
kamilsmr <kamils6035@gmail.com>
2022-10-17 09:26:02 +0300

    logic

--- app.js
+++ app.js
@@ -1,2 +1,24 @@
+const coinArray = ["Heads","Tails"];
 const message = document.querySelector(".message");
-const buttons = document.querySelectorAll("button")
+const buttons = document.querySelectorAll("button");
+
+for(let i =0; i<buttons.length; i++){
+console.log(buttons[i]);
+buttons[i].addEventListener("click",lossCoin)
+}
+
+function lossCoin(e){
+    // console.log("player:" + e.target.innetText);
+    let playerGuess = e.target.innerText;
+    let computerToss = Math.floor(Math.random()*2);
+    let computerGuess = coinArray[computerToss];
+    // console.log("computer:" + coinArray[computerToss]);
+    if(playerGuess){
+
+    }else{
+
+    }
+}
+
+
+
